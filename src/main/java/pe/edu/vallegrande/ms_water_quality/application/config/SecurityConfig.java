c52b9188b220f6dce32089a9f8c0be855db4a9c9
RonaldinhoCc <ronaldinho.ccencho@vallegrande.edu.pe>
2025-07-31 21:12:16 -0500

    Security Config keycloak

---
+++ src/main/java/pe/edu/vallegrande/ms_water_quality/application/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package pe.edu.vallegrande.ms_water_quality.application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableReactiveMethodSecurity
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeExchange(authorize -> authorize
+                .anyExchange().authenticated()
+            )
+            .oauth2ResourceServer(resourceServer -> resourceServer
+                .jwt(jwt -> {})
+            );
+
+        return http.build();
+    }
+}
